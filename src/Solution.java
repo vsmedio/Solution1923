a23368e94451f1b76520578ddc8d6413e0ef64fd
Arvis Abromovics <info@vsmedio.lv>
2018-01-11 22:20:06 +0100

    Initial commit

---
+++ src/Solution.java
@@ -0,0 +1,37 @@
+/* 
+Слова с цифрами
+В метод main первым параметром приходит имя файла1, вторым - файла2.
+Файл1 содержит строки со словами, разделенные пробелом.
+Записать через пробел в Файл2 все слова, которые содержат цифры, например, а1 или abc3d.
+Закрыть потоки.
+
+
+Требования:
+1. Программа НЕ должна считывать данные с консоли.
+2. Программа должна считывать содержимое первого файла (используй FileReader c конструктором String).
+3. Поток чтения из файла (FileReader) должен быть закрыт.
+4. Программа должна записывать во второй файл все слова из первого файла которые содержат цифры (используй FileWriter).
+5. Поток записи в файл (FileWriter) должен быть закрыт.
+*/
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(args[0]));
+        BufferedWriter writer = new BufferedWriter(new FileWriter(args[1]));
+        while (reader.ready()) {
+            String line = reader.readLine();
+            for (String str : line.split(" "))
+                if (!str.matches("^\\D*$"))
+                    writer.write(str + " ");
+        }
+        reader.close();
+        writer.close();
+    }
+}
